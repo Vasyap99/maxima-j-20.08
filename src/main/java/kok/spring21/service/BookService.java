f004f2528d35fea39d13c52baf1652fbfcd6495f
kko <kkl24@mail.ru>
2024-08-25 20:54:30 +0300

    3+service#

---
+++ src/main/java/kok/spring21/service/BookService.java
@@ -0,0 +1,14 @@
+package kok.spring21.service;
+
+import kok.spring21.repo.*;
+import kok.spring21.models.*;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class BookService{
+    BookRepository bookRepository;
+
+    public void save(Book book){
+        bookRepository.save(book);
+    }
+}
