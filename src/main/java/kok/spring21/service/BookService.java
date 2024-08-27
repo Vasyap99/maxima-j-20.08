942aa02aabc4b9d7e528544b003bff473a33f826
kko <kkl24@mail.ru>
2024-08-27 22:01:25 +0300

    6.2-crud#

--- src/main/java/kok/spring21/service/BookService.java
+++ src/main/java/kok/spring21/service/BookService.java
@@ -4,12 +4,31 @@ import kok.spring21.repo.*;
 import kok.spring21.models.*;
 import lombok.AllArgsConstructor;
 
+import java.util.List;
+
+
 @AllArgsConstructor
 public class BookService{
     BookRepository bookRepository;
 
     public void save(Book book){
         bookRepository.save(book);
+    }
+
+    public List<Book> getBooks() {
+        return bookRepository.getAllBooks();
+    }
+
+    public Book getBookById(int id) {
+        return bookRepository.findById(id);
+    }
+
+    public void deleteBook(int id) {
+        bookRepository.delete(id);
+    }
+
+    public Book updateBook(Book book) {
+       return bookRepository.update(book);
     }
 
     public String toString(){
