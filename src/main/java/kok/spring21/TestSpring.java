ef991013676e47c2755637851bd306ae128d5ecd
kko <kkl24@mail.ru>
2024-08-27 22:40:03 +0300

    6.3-crud

--- src/main/java/kok/spring21/TestSpring.java
+++ src/main/java/kok/spring21/TestSpring.java
@@ -6,6 +6,8 @@ import kok.spring21.repo.*;
 import kok.spring21.models.*;
 import kok.spring21.service.*;
 
+import java.util.Scanner;
+
 
 public class TestSpring {
     public static void main(String[] args) {
@@ -35,7 +37,54 @@ public class TestSpring {
                              .build() 
         );
 
-        System.out.println(bookService.toString());
-        context.close();
+        Scanner s=new Scanner(System.in);
+        int id=0;
+        Book b=null;
+        String n=null,a=null;
+        while(true){
+            System.out.println(bookService.toString());
+            System.out.println("<Enter command: C R U D, E for exit>");
+                String i=s.nextLine();
+                switch(i){
+                    case "C":
+                        System.out.println("-Enter name:");
+                        n=s.nextLine();
+                        System.out.println("-Enter author:");
+                        a=s.nextLine();
+                        bookService.save(Book.builder()
+                             .name(n)
+                             .author(a)
+                             .build() 
+                        );
+                        break;
+                    case "R":
+                        System.out.println("-Enter id:");
+                        id=Integer.parseInt(s.nextLine());
+                        b=bookService.getBookById(id);
+                        System.out.println(b.toString());
+                        break;
+                    case "U":
+                        System.out.println("-Enter id:");
+                        id=Integer.parseInt(s.nextLine());
+                        System.out.println("-Enter name:");
+                        n=s.nextLine();
+                        System.out.println("-Enter author:");
+                        a=s.nextLine();
+                        bookService.updateBook(new Book(id,n,a));                        
+                        break;
+                    case "D":
+                        System.out.println("-Enter id:");
+                        id=Integer.parseInt(s.nextLine());
+                        bookService.deleteBook(id);                       
+                        break;
+                    case "E":
+                        context.close();
+                        return;
+                    default:
+                        ;
+                }
+        }
+
+        //context.close();
     }
 }
